7e3fd71ea377d20008adcda76771cd6b46aa3770
pranav2014 <pranavyadav2014@gmail.com>
2019-07-28 15:26:00 +0530

    Zipkin implemented

---
+++ zipkin-client-service/src/main/java/com/example/zipkinclientservice/ZipkinClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.zipkinclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinClientServiceApplication.class, args);
+	}
+
+}
---
+++ zipkin-client-service/src/test/java/com/example/zipkinclientservice/ZipkinClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zipkinclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
